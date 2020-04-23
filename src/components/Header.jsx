d7a641e1a9dd30e37842cbaf9e98ecfef99869c4
Your Name <alakai20136@gmail.com>
2020-04-23 18:33:52 +0300

    header, navbar, profile components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="header">
+            <img src='https://cdn.worldvectorlogo.com/logos/react-2.svg'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav className="nav">
+            <div>Profile</div>
+            <div>Messages</div>
+            <div>New</div>
+        </nav>
+    );
+}
+
+export default Navbar;
